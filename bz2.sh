505e1f1d51736cc49ec50c3ec69acebe973d82ba
fafa <842355358@qq.com>
2019-03-29 22:14:28 +0000

    upload

--- bz2.sh
+++ bz2.sh
@@ -1,12 +1,10 @@
 #/usr/bin/sh
-bzip2 -9 *
+git pull origin master
 bzip2 -9 */*
 bzip2 -9 */*/*
 bzip2 -9 */*/*/*
 bzip2 -9 */*/*/*/*
 bzip2 -9 */*/*/*/*/*
-bzip2 -d bz2.sh.bz2
-bzip2 -d CNAME.bz2
 git add . 
 git commit -m "upload"
 git push origin master
