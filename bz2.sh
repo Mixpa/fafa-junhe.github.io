9b6e7ea1452f531ccbee26b94301f4cb6e7164b0
fafa_junhe <842355358@qq.com>
2019-04-26 23:06:36 +0800

    upload

--- bz2.sh
+++ bz2.sh
@@ -1,5 +1,6 @@
 #/usr/bin/sh
 git pull origin master
+mv .git ../
 find . -name index.html -exec rm -rvf {} \;
 bzip2 -9 */*
 bzip2 -9 */*/*
@@ -7,6 +8,7 @@ bzip2 -9 */*/*/*
 bzip2 -9 */*/*/*/*
 bzip2 -9 */*/*/*/*/*
 indexme -r
+mv ../.git ./
 git add . 
 git commit -m "upload"
 git push origin master
